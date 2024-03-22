4c0c954ddb7aefe3c41250ea72510a891eedcf6f
nashidashafin <naashidap@gmail.com>
2024-03-22 15:45:01 +0530

    initial commit

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function displayData(n){
+    result.value+=n
+}
+function allClear() {
+    result.value=""
+    
+}
+function backSpace(){
+    result.value= result.value.slice(0,-1)
+}
+function findOut() {
+    result.value=eval(result.value)
+    
+}
